58c5d7d972fb2add4d3c69ed351ab3391a9e3cb0
Waylon S. Walker <waylon@waylonwalker>
2025-05-16 09:37:08 -0500

    update screenkey timing

--- ninesui/__init__.py
+++ ninesui/__init__.py
@@ -517,7 +517,7 @@ class NinesUI(App):
     def on_key(self, event):
         key = event.key
         if SCREENKEY:
-            self.notify(f"key: {key}", timeout=2)
+            self.notify(f"key: {key}", timeout=2, severity="key")
 
         if key == "J":
             self.router.jump_owner()
